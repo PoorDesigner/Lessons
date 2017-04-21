73a1c8c06671fb82c0185a1d6cc1ac55bfbcbc57
PoorDesigner <vivekh.meka@gmail.com>
2017-04-21 21:35:27 +0530

    Initial

---
+++ AbstractVsInterfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractVsInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Interview
+    {
+        void AskIntro()
+        {
+
+        }
+    }
+    class Fresher: Interview
+    {
+
+    }
+}
