53ef610f697b5de0a814bbb2148508dfaa49972c
PoorDesigner <vivekh.meka@gmail.com>
2017-04-21 21:53:17 +0530

    3

--- AbstractVsInterfaces/Program.cs
+++ AbstractVsInterfaces/Program.cs
@@ -14,13 +14,41 @@ namespace AbstractVsInterfaces
     }
     abstract class Interview
     {
-        void AskIntro()
+        void WrittenTest()
         {
-
+            Console.Write("Written test");
         }
+
+        void TellMeAboutYourself()
+        {
+            Console.Write("Ask to tell about himself");
+        }
+
+        /// <summary>
+        /// Should be publis
+        /// Should not have implementation details
+        /// </summary>
+        /// <returns></returns>
+        public abstract bool TechnicalQuestions();
+
+        public abstract bool Hr();
+
     }
     class Fresher: Interview
     {
+        /// <summary>
+        /// Should override the abstract methods of the Ingterview
+        /// </summary>
+        /// <returns></returns>
+        public override bool TechnicalQuestions()
+        {
+            throw new NotImplementedException();    
+        }
 
+        public override bool Hr()
+        {
+            throw new NotImplementedException();
+        }
     }
+   
 }
